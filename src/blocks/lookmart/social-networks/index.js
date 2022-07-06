a89d737f0da3c1a09ce0d8085927263aa3ae7a0f
Vera Vlasova <vlasovavery@gmail.com>
2022-07-06 12:07:53 +0500

    fix(social media): Hides mail in the mobile version [MINOR] Скроет почту в блоке соц.сети, мв
    https://jira.railsc.ru/browse/SG-7679

--- src/blocks/lookmart/social-networks/index.js
+++ src/blocks/lookmart/social-networks/index.js
@@ -2,3 +2,4 @@ import './social-networks.css';
 import './social-networks_small.css';
 import './social-networks_old.css';
 import './social-networks_expert.css';
+import './social-networks_hide.css';
